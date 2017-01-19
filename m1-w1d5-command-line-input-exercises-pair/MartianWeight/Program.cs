6987cad01c40aa63841f688a25160c602de6ac54
John Fulton <john@TechElevator.com>
2017-01-19 17:55:01 -0500

    m1-w1d5-command-line-pairs

---
+++ m1-w1d5-command-line-input-exercises-pair/MartianWeight/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MartianWeight
+{
+    class Program
+    {
+        /*
+        In case you've ever pondered how much you weight on Mars, here's the calculation:
+ 	    Wm = We* 0.378
+ 	    where 'Wm' is the weight on Mars, and 'We' is the weight on Earth
+
+        Write a command line program which accepts a series of Earth weights from the user  
+        and displays each Earth weight as itself, and its Martian equivalent.
+
+
+        C:\Users> MartianWeight  
+        Enter a series of Earth weights (space-separated): 98 235 185
+            
+        98 lbs.on Earth, is 37 lbs.on Mars.
+        235 lbs.on Earth, is 88 lbs.on Mars.
+        185 lbs.on Earth, is 69 lbs.on Mars. 
+        */
+        static void Main(string[] args)
+        {
+        }
+    }
+}
